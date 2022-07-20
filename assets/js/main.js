5c258859d1260592347e32e62a34c023c4e5eeed
iaguirre19 <iran.agui1993@gmail.com>
2022-07-20 06:06:03 -0500

    desktop terminada

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Mail
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = "EMAIL";
+  ftypes[0] = "email";
+  fnames[1] = "FNAME";
+  ftypes[1] = "text";
+  fnames[2] = "LNAME";
+  ftypes[2] = "text";
+  fnames[3] = "ADDRESS";
+  ftypes[3] = "address";
+  fnames[4] = "PHONE";
+  ftypes[4] = "phone";
+  fnames[6] = "MMERGE6";
+  ftypes[6] = "dropdown";
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
+
